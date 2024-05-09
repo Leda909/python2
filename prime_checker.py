def2d420d6dbe9df449b29e2059f62662d30b583
Leda909 <65784743+Leda909@users.noreply.github.com>
2024-05-09 13:20:02 +0100

    Create prime_checker.py

---
+++ prime_checker.py
@@ -0,0 +1,30 @@
+# Welcome message
+print("Welcome to the Prime Checker!")
+
+while True:
+  try:
+    #Get a number from the user
+    number = int(input("Please enter a whole number: "))
+    
+    #Check if the number is prime
+    if number<2:
+      prime = False
+    else:
+      prime = True
+      divisor = 2
+      while divisor * divisor <= number:
+        if number % divisor == 0:
+          prime = False
+          break
+        divisor += 1
+
+    # Show the Result
+    if prime:
+        print(f"{number} is a prime number.")
+    else:
+        print(f"{number} is not a prime number.")
+    break
+
+  # Handel error
+  except ValueError:
+    print("That's not a number. Please enter a whole number.")
